2b3e121164c0e164fd1466bbc60ea25851f81010
Matt Johnson <matt@mattjohnson.io>
2016-09-23 09:51:38 +0100

    Add editorconfig and match existing indentation.

--- gulpfile.js
+++ gulpfile.js
@@ -21,99 +21,102 @@ var app    = assemble(),
 // Configuration
 
 var paths = {
-  files: ['./app/CNAME'],
-  html:  ['./app/views'],
-  img:   ['./app/assets/img/**/*'],
-  css:   [
-    './node_modules/bootstrap/dist/css/bootstrap.min.css',
-    './app/assets/css/**/*.css'
-  ]
+    files: ['./app/CNAME'],
+    html:  ['./app/views'],
+    img:   ['./app/assets/img/**/*'],
+    css:   [
+        './node_modules/bootstrap/dist/css/bootstrap.min.css',
+        './app/assets/css/**/*.css'
+    ]
 };
 
 
 // Tasks
 
 gulp.task('browser-sync', function() {
-  browserSync({
-    server: {
-      baseDir: './dist'
-    }
-  });
+    browserSync({
+        server: {
+            baseDir: './dist'
+        }
+    });
 });
 
 gulp.task('clean', function() {
-  return del(['./dist']);
+    return del(['./dist']);
 });
 
 gulp.task('compile', ['clean'], function() {
-  runSequence(['files', 'html', 'images', 'stylesheets']);
+    runSequence(['files', 'html', 'images', 'stylesheets']);
 });
 
 gulp.task('images', function() {
-  return gulp.src(paths.img)
+    return gulp.src(paths.img)
     .pipe(imagemin({
-      multipass: true,
-      svgoPlugins: [{collapseGroups: false, removeViewBox: false}]
+        multipass: true,
+        svgoPlugins: [{collapseGroups: false, removeViewBox: false}]
     }))
     .pipe(gulp.dest('./dist/img'));
 });
 
 gulp.task('load', function(cb) {
-  app.layouts(paths.html + '/layouts/*.hbs');
-  app.pages(paths.html  + '/*.hbs');
-  app.partials(paths.html + '/partials/*.hbs');
-  app.option('layout', 'default.hbs');
-  cb();
+    app.layouts(paths.html + '/layouts/*.hbs');
+    app.pages(paths.html  + '/*.hbs');
+    app.partials(paths.html + '/partials/*.hbs');
+    app.option('layout', 'default.hbs');
+    cb();
 });
 
 gulp.task('stylesheets', function() {
-  return gulp.src(paths.css)
+    return gulp.src(paths.css)
     .pipe(autoprefixer())
     .pipe(gulp.dest('./dist/css'))
     .pipe(reload({stream: true}));
 });
 
 gulp.task('files', function() {
-  return gulp.src(paths.files)
+    return gulp.src(paths.files)
     .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(paths.html + '/**/*.hbs', ['html']);
-  gulp.watch(paths.img,  ['images']);
-  gulp.watch(paths.css,  ['stylesheets']);
+    gulp.watch(paths.html + '/**/*.hbs', ['html']);
+    gulp.watch(paths.img,  ['images']);
+    gulp.watch(paths.css,  ['stylesheets']);
 
-  gulp.watch('./dist/*.html').on('change', reload);
+    gulp.watch('./dist/*.html').on('change', reload);
 });
 
 gulp.task('html', ['load'], function () {
-  return app.toStream('pages')
+    return app.toStream('pages')
     .pipe(app.renderFile())
     .pipe(extname())
     .pipe(app.dest('dist'));
 });
 
 gulp.task('rev', function() {
-  var revAll = new RevAll({dontRenameFile: [/\.html/, /CNAME$/, /humans\.txt/], dontUpdateReference: [/humans\.txt/]});
+    var revAll = new RevAll({
+        dontRenameFile: [/\.html/, /CNAME$/, /humans\.txt/],
+        dontUpdateReference: [/humans\.txt/]
+    });
 
-  gulp.src('dist/**')
-  .pipe(revAll.revision())
-  .pipe(size({
-    showFiles: true,
-    gzip: true
-  }))
-  .pipe(gulp.dest('dist'));
+    gulp.src('dist/**')
+    .pipe(revAll.revision())
+    .pipe(size({
+        showFiles: true,
+        gzip: true
+    }))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('dev', function() {
-  runSequence('compile', 'browser-sync', 'watch');
+    runSequence('compile', 'browser-sync', 'watch');
 });
 
 gulp.task('build', ['clean'], function() {
-  runSequence(['files', 'html', 'images', 'stylesheets'], 'rev');
+    runSequence(['files', 'html', 'images', 'stylesheets'], 'rev');
 });
 
 gulp.task('deploy', function() {
-  return gulp.src('./dist/**/*')
+    return gulp.src('./dist/**/*')
     .pipe(ghPages());
 });
